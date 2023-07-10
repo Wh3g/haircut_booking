896513bdcfc893ba48f54f476bf563790d661272
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-07-10 17:02:44 +0100

    create booking endpoint created

--- haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/BookingRestController.java
+++ haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/BookingRestController.java
@@ -3,6 +3,8 @@ package com.wh.haircutbooking.restcontrollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wh.haircutbooking.entities.Booking;
@@ -14,7 +16,8 @@ public class BookingRestController {
 	@Autowired
 	private BookingService service;
 
-	public ResponseEntity<Booking> createBooking(Booking booking) {
+	@PostMapping("/bookings")
+	public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
 		Booking storedBooking = service.createBooking(booking);
 		return new ResponseEntity<Booking>(storedBooking, HttpStatus.CREATED);
 	}
