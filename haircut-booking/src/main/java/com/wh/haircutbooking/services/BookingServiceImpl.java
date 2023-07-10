f161795ff4c98a021dcf8550f388bfadf93d8fcb
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-07-10 16:40:00 +0100

    rest controller calls service method

--- haircut-booking/src/main/java/com/wh/haircutbooking/services/BookingServiceImpl.java
+++ haircut-booking/src/main/java/com/wh/haircutbooking/services/BookingServiceImpl.java
@@ -1,10 +1,15 @@
 package com.wh.haircutbooking.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.wh.haircutbooking.entities.Booking;
 import com.wh.haircutbooking.repositories.BookingRepository;
 
+@Service
 public class BookingServiceImpl implements BookingService {
 
+	@Autowired
 	private BookingRepository repository;
 
 	@Override
