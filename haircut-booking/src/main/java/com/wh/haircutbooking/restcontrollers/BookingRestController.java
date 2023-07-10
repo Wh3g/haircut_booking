085265cbd1ff17ca6b20cc9d2243721d28fee435
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-07-10 17:30:29 +0100

    get all bookings endpoint created

--- haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/BookingRestController.java
+++ haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/BookingRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class BookingRestController {
 		return new ResponseEntity<Booking>(storedBooking, HttpStatus.CREATED);
 	}
 
+	@GetMapping("/bookings")
 	public List<Booking> getAllBookings() {
 		return service.getAllBookings();
 	}
