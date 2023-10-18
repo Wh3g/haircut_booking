13a3446770be3123ec39c5f617b93458b4908a0e
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-10-18 16:27:51 +0100

    create category endpoint done

--- haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/CategoryRestController.java
+++ haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/CategoryRestController.java
@@ -3,16 +3,21 @@ package com.wh.haircutbooking.restcontrollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wh.haircutbooking.entities.Category;
 import com.wh.haircutbooking.services.CategoryService;
 
+@RestController
 public class CategoryRestController {
 
 	@Autowired
 	private CategoryService service;
 
-	public ResponseEntity<Category> createCategory(Category category) {
+	@PostMapping("/categories")
+	public ResponseEntity<Category> createCategory(@RequestBody Category category) {
 		Category storedCategory = service.createCategory(category);
 
 		return new ResponseEntity<Category>(storedCategory, HttpStatus.CREATED);
--- haircut-booking/src/main/java/com/wh/haircutbooking/services/CategoryServiceImpl.java
+++ haircut-booking/src/main/java/com/wh/haircutbooking/services/CategoryServiceImpl.java
@@ -1,10 +1,12 @@
 package com.wh.haircutbooking.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.wh.haircutbooking.entities.Category;
 import com.wh.haircutbooking.repositories.CategoryRepository;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
