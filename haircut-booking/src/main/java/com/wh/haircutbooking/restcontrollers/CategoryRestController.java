520d634799190e9e8f9b574395322f44adef7b44
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-12-06 00:46:18 +0000

    get all categories endpoint

--- haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/CategoryRestController.java
+++ haircut-booking/src/main/java/com/wh/haircutbooking/restcontrollers/CategoryRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class CategoryRestController {
 		return new ResponseEntity<Category>(storedCategory, HttpStatus.CREATED);
 	}
 
+	@GetMapping("/categories")
 	public List<Category> getAllCategories() {
 		return service.getAllCategories();
 	}
